b5fa9b4abc2c08a9047bc516c78c9593a88eb4ca
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-03-06 00:27:42 -0600

    Improve ingredients pulling to separate units from measurement

---
+++ lists.py
@@ -0,0 +1,2 @@
+descriptors = ['fresh', 'frozen', 'dried', 'minced', 'boneless', 'skinless', 'ground']
+preparation = ['finely chopped', 'minced', 'diced']
--- recipe.py
+++ recipe.py
@@ -1,5 +1,7 @@
 import BeautifulSoup as bs
 import urllib2
+import lists
+import string
 
 class recipe:
     ingredients = []
@@ -16,8 +18,41 @@ class recipe:
         i = 0
         for ing in ingredients:
         	self.ingredients.append({})
-        	self.ingredients[i]['amount'] = ing.find('span', {'class': 'ingredient-amount'}).string
-        	self.ingredients[i]['name'] = ing.find('span', {'class': 'ingredient-name'}).string
+        	amount = ing.find('span', {'class': 'ingredient-amount'}).string
+        	amount = amount.split(" ")
+        	if len(amount) == 1:
+        		self.ingredients[i]['quantity'] = amount[0]
+        		self.ingredients[i]['measurement'] = ''
+        	elif len(amount) == 2:
+        	    self.ingredients[i]['quantity'] = amount[0]
+        	    self.ingredients[i]['measurement'] = amount[1]
+        	if not self.ingredients[i]['quantity'].isdigit():
+        		if len(self.ingredients[i]['quantity'].split('/')) == 2:
+        			vals = self.ingredients[i]['quantity'].split('/')
+        			self.ingredients[i]['quantity'] = float(vals[0])/float(vals[1])
+        	name = ing.find('span', {'class': 'ingredient-name'}).string
+        	name2 = "".join(l for l in name if l not in string.punctuation)
+        	print name2
+        	nameArr = name2.split(' ')
+        	descriptors = []
+        	nameArr2 = []
+        	for word in nameArr:
+        		if word in lists.descriptors:
+        			descriptors.append(word)
+        		else:
+        			nameArr2.append(word)
+        	desc = ''
+        	for j in range(len(descriptors)):
+        		desc += descriptors[j]
+        		if j != len(descriptors)-1:
+        			desc += ' '
+        	self.ingredients[i]['descriptor'] = desc
+        	name = ''
+        	for j in range(len(nameArr2)):
+        		name += nameArr2[j]
+        		if j != len(nameArr2)-1:
+        			name += ' '
+        	self.ingredients[i]['name'] = name
         	i += 1
         return self.ingredients
 
