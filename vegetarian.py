75fcd73930fcf6cbff0001306c8ff286f1a1625e
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-03-15 16:35:34 -0500

    Working through bugs from that commit

--- vegetarian.py
+++ vegetarian.py
@@ -5,11 +5,13 @@ def tovegetarian(recipe):
 	#identify protein (to remove or substitute)
 	meats = []
 	for ingredient in recipe.ingredients:
-		for meat in proteins:
+		for meat in lists.proteins:
 			if meat in ingredient["name"]:
 				meats.append(ingredient)
+				break
 	# remove meat from recipe
 	for meat in meats:
+		print meat
 		recipe.ingredients.remove(meat)
 
 	return recipe
