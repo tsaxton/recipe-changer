93194b28f6dc4a9538f77d40e39cb1a1addc4104
selberts <samuel.elberts@gmail.com>
2014-03-16 15:51:06 -0500

    adding list

--- Healthy.py
+++ Healthy.py
@@ -8,26 +8,10 @@ def tohealthy(recipe):
 	#identify protein (to remove or substitute)
 	meats = []
 	for ingredient in recipe.ingredients:
-		for meat in lists.healthy:
-			if meat in ingredient["name"]:
-				meats.append(ingredient)
+		for ingredients in lists.healthy:
+			if ingredients in ingredient["name"]:
+				ingredient["name"] = lists.healthy[ingredients]
 				break
-	# remove meat from recipe
-	for meat in meats:
-		print meat
-		recipe.ingredients.remove(meat)
-
-
-	type = recipe.getCuisineType()
-
-	if type == "american":
-		recipe.ingredients.append({'name':'mushroom'})
-	elif type == "italian":
-		recipe.ingredients.append({'name':'eggplant'})
-	elif type == "asian":
-		recipe.ingredients.append({'name':'tofu'})
-	elif type == "mexican":
-		recipe.ingredients.append({'name':'peppers'})
 
 
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import recipe
 import pprint
 import sys
 import vegetarian
+import healthy
 url = sys.argv[1]
 a = recipe.recipe(url)
 pprint.pprint(a.getIngredients())
@@ -9,6 +10,8 @@ pprint.pprint(a.getSteps())
 a = vegetarian.tovegetarian(a)
 print a.getCuisineType()
 pprint.pprint(a.getIngredients())
+a = a.healthy.tohealthy(a)
+pprint.pprint(a.getIngredients)
 #pprint.pprint(a.getSteps())
 #pprint.pprint(a.getTools())
 #print(a.getPrimaryMethod())
