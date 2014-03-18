8fb123d0f9b7a410c9f008cd17aaa2e77ab01ba0
juliuszc <juliuszchoinski2015@u.northwestern.edu>
2014-03-17 20:51:39 -0500

    Healthy stuff

--- Healthy.py
+++ Healthy.py
@@ -5,6 +5,15 @@ import recipe
 import lists
 
 def tohealthy(recipe):
+        #change cooking method to healthy alternative
+        for i in range(len(recipe.steps)):
+            method = recipe.steps[i]
+            for methods in lists.healthy:
+                if methods in method["action"]:
+                    recipe.steps[i]["action"] = [lists.healthy[methods]]
+                    recipe.steps[i]["tools"] = ["baking pan"]
+                    break
+        
 	#identify protein (to remove or substitute)
 	for i in range(len(recipe.ingredients)):
             ingredient = recipe.ingredients[i]
