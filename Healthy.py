cae45be87ffe4fddf52e5d2ab1cabf51a6a06a13
selberts <samuel.elberts@gmail.com>
2014-03-18 17:17:06 -0500

    Me and Jules fixed ingredients in steps for healthy

--- Healthy.py
+++ Healthy.py
@@ -6,11 +6,12 @@ import lists
 
 def tohealthy(recipe):
         #change cooking method to healthy alternative
+	##need to change primary method tag
         for i in range(len(recipe.steps)):
             method = recipe.steps[i]
             for methods in lists.healthy:
                 if methods in method["action"]:
-                    recipe.steps[i]["action"] = [lists.healthy[methods]]
+                    recipe.steps[i]["action"] = lists.healthy[methods]
                     recipe.steps[i]["tools"] = ["baking pan"]
                     break
         
@@ -19,10 +20,17 @@ def tohealthy(recipe):
             ingredient = recipe.ingredients[i]
 	    for ingredients in lists.healthy:
 		if ingredients in ingredient["name"]:
-		    recipe.ingredients[i]["name"] = lists.healthy[ingredients]
-		    break
-
-
+			recipe.swapStepIngredients(recipe.ingredients[i]["name"],lists.healthy[ingredients])
+			recipe.ingredients[i]["name"] = lists.healthy[ingredients]
+			break
+	## cut down unhealthy ingredient amounts
+	## caramelizing
+	## stir fry no oil
+	## cut down pasta and increase veggies
+	## cut out mayo
+	## swap mozarella for skim ricotta
+	## salad dressings - blue cheese, marinades
+	## couple table spoons of broth can be used for oil in stir fry
 
 	return recipe
 
