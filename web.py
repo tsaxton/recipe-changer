125ede6bfb5d2cb10fc6f019905afa628798eb36
Thomas M. Saxton <thomasmsaxton@gmail.com>
2014-03-18 16:34:25 -0500

    Using web interface to transform recipes

--- web.py
+++ web.py
@@ -2,6 +2,8 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 import os
 import recipe as recipeClass
+import vegetarian
+import cuisinetype
 
 app=Flask(__name__)
 app.config.from_object(__name__)
@@ -19,11 +21,34 @@ def promptUser():
 
 @app.route('/originalRecipe', methods=['GET','POST'])
 def getOriginalRecipe():
+    recipe = None
     try:
         recipe = recipeClass.recipe(request.form['url'])
     except:
         return render_template('promptUser.html', error='There was a problem with the recipe you entered. Either the recipe was not from allrecipes.com, or there was a malformed expression within the recipe.')
-    return render_template('displayRecipe.html', url=request.form['url'], recipe=recipe)
+    return render_template('displayRecipe.html', url=request.form['url'], recipe=recipe, transform='', error='')
+
+@app.route('/transform', methods=['GET', 'POST'])
+def transformRecipe():
+    recipe = None
+    url = request.form['url']
+    transformation = request.form['transformation']
+    try:
+        recipe = recipeClass.recipe(url)
+    except:
+        return render_template('promptUser.html', error='There was a problem retrieving the original recipe. Your internet connection may be down.')
+    try:
+        if transformation=='vegetarian':
+            newRecipe = vegetarian.tovegetarian(recipe)
+        elif transformation=='healthy':
+            newRecipe = recipe # do this for now until know how to run the healthy transformation
+        elif transformation=='american' or transformation=='mexican' or transformation=='italian' or transformation=='asian':
+            newRecipe = cuisinetype.changetype(recipe, transformation)
+        else:
+        	return render_template('displayRecipe.html', url=url, recipe=recipe, transform='', error='There was a problem transforming your recipe. The original recipe is displayed below.')
+    except:
+        return render_template('displayRecipe.html', url=url, recipe=recipe, transform='', error='There was a problem transforming your recipe. The original recipe is displayed below.')
+    return render_template('displayRecipe.html', url=url, recipe=newRecipe, transform=transformation, error='')
 
 
 if __name__ == '__main__':
